5c382b8449d1f382f04a3f9678e6851f31d36c0a
marsonshine <ms27946@163.com>
2023-03-02 15:18:52 +0800

    feat: asynclocal threadlocal

---
+++ Threads/AsyncLocalDemo/IValueAccessor.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace AsyncLocalDemo
+{
+    public interface IValueAccessor<T>
+    {
+        T Value { get; set; }
+    }
+
+    public class ValueAccessor<T> : IValueAccessor<T>
+    {
+        private static AsyncLocal<ValueHolder<T>> _asyncLocal = new();
+        public T Value
+        {
+            get => _asyncLocal.Value != null ? _asyncLocal.Value.Value : default; 
+            set
+            {
+                _asyncLocal.Value ??= new ValueHolder<T>();
+                _asyncLocal.Value.Value = value;
+            }
+        }
+    }
+
+    class ValueHolder<T>
+    {
+        [MaybeNull]
+        public T Value { get; set; }
+    }
+}
---
+++ Threads/AsyncLocalDemo/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AsyncLocalDemo;
+
+_asyncLocal.Value = "A";
+
+Console.WriteLine($"AsyncLocal before FooAsync: {_asyncLocal.Value}");
+
+await FooAsync();
+
+Console.WriteLine($"AsyncLocal after await FooAsync: {_asyncLocal.Value}");
+
+_threadLocal.Value = "a";
+Console.WriteLine($"AsyncLocal before BarAsync: {_threadLocal.Value}");
+await BarAsync();
+Console.WriteLine($"AsyncLocal before BarAsync: {_threadLocal.Value}");
+
+_valueAccessor.Value = "1";
+Console.WriteLine($"ValueAccessor before await FooBarAsync in Main: {_valueAccessor.Value}");
+
+await FooBarAsync();
+
+Console.WriteLine($"ValueAccessor after await FooBarAsync in Main: {_valueAccessor.Value}");
+
+
+
+public partial class Program
+{
+    private static AsyncLocal<string> _asyncLocal = new AsyncLocal<string>();
+
+    private static async Task FooAsync()
+    {
+        _asyncLocal.Value = "B";
+
+        Console.WriteLine($"AsyncLocal before await in FooAsync: {_asyncLocal.Value}");
+
+        await Task.Delay(100);
+
+        Console.WriteLine($"AsyncLocal after await in FooAsync: {_asyncLocal.Value}");
+    }
+
+    private static ThreadLocal<string> _threadLocal = new ThreadLocal<string>();
+
+    private static async Task BarAsync()
+    {
+        _threadLocal.Value = "b";
+
+        Console.WriteLine($"AsyncLocal before await in BarAsync: {_threadLocal.Value}");
+
+        await Task.Delay(100);
+
+        Console.WriteLine($"AsyncLocal after await in BarAsync: {_threadLocal.Value}");
+    }
+
+    private static IValueAccessor<string> _valueAccessor = new ValueAccessor<string>();
+    private static async Task FooBarAsync()
+
+    {
+        _valueAccessor.Value = "2";
+
+        Console.WriteLine($"ValueAccessor before await in FooBarAsync: {_valueAccessor.Value}");
+
+        await Task.Delay(100);
+
+        Console.WriteLine($"ValueAccessor after await in FooBarAsync: {_valueAccessor.Value}");
+
+    }
+}
+
+
