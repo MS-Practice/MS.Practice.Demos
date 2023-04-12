6237224a50df0bde3a8f97a0ffe0ddc231aa1e0d
marsonshine <ms27946@163.com>
2023-04-12 10:02:55 +0800

    fix: origin sse implement in net

--- ServerSendEvent/Program.cs
+++ ServerSendEvent/Program.cs
@@ -1,6 +1,10 @@
 using Lib.AspNetCore.ServerSentEvents;
 using Microsoft.AspNetCore.ResponseCompression;
 using ServerSendEvent.HostServices;
+using System.Buffers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Unicode;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,6 +42,42 @@ app.UseResponseCompression()
         endpoint.MapControllers();
         //endpoint.MapServerSentEvents("/see-heartbeat");
         endpoint.MapServerSentEvents("/chat-completion");
+        endpoint.Map("/origin-sse", async (context) =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string txt = $"Demo.AspNetCore.ServerSentEvents Heartbeat ({DateTime.UtcNow} UTC)";
+                context.Response.ContentType = "text/event-stream";
+                string dataField = $"data:{txt}\n\n"; // \n\n 表示结束
+                await context.Response.Body.WriteAsync(Encoding.UTF8.GetBytes(dataField));
+                await context.Response.Body.FlushAsync();
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        });
+        endpoint.Map("/origin-sse2", async (context) =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                context.Response.ContentType = "text/event-stream";
+                string dataField = $"""
+data:Demo.AspNetCore.ServerSentEvents Heartbeat ({DateTime.UtcNow} UTC)
+id:{RandomNumberGenerator.GetInt32(10000,99999)}
+type:message
+
+
+""";
+                // 上述格式不好看，可以修正以下：
+                string dataField2 = $"""
+data:Demo.AspNetCore.ServerSentEvents Heartbeat ({DateTime.UtcNow} UTC)
+id:{RandomNumberGenerator.GetInt32(10000, 99999)}
+type:message
+""";
+                dataField2 += "\n\n";
+                await context.Response.Body.WriteAsync(Encoding.UTF8.GetBytes(dataField));
+                await context.Response.Body.FlushAsync();
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        });
     })
     ;
 
